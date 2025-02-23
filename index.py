73f61c0cb20a4c1f25a9cb305789350766ce8c6e
SC0624a <97958094+SC0624a@users.noreply.github.com>
2025-02-23 19:57:34 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,28 @@
+from importlib import reload
+import asyncio
+import msg_data
+import websockets
+import json
+import fn
+
+
+ws_url = 'ws://127.0.0.1:3001'
+
+async def main():
+    try:
+        async with websockets.connect(f"{ws_url}/event") as ws:
+            while True:
+                message = await ws.recv()
+                message = json.loads(message)
+                await msg_data.messages(message)
+                reload(fn)
+    except Exception as e:
+        print(f'{e}')
+    finally:
+        await asyncio.sleep(5)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except:
+        pass
